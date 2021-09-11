8f427e3e2077e374ae7acc5116d74e770c6e5e32
buanet <andre@buanet.de>
2021-09-11 19:42:40 +0200

    testing

--- stage-iobroker/01-install-iobroker/00-run.sh
+++ stage-iobroker/01-install-iobroker/00-run.sh
@@ -2,10 +2,12 @@
 
 # run iobroker install script
 on_chroot << EOF
+echo "curl -sLf https://iobroker.net/install.sh | bash -"
 curl -sLf https://iobroker.net/install.sh | bash -
 EOF
 
 # enable iobroker autostart
 on_chroot << EOF
+echo "update-rc.d iobroker.sh defaults"
 update-rc.d iobroker.sh defaults
 EOF
