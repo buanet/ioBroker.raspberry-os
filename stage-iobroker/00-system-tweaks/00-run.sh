cd15a0a4d47689586f84e2d66ee6fffa97283f5a
buanet <andre@buanet.de>
2021-09-12 14:11:15 +0200

    testing

--- stage-iobroker/00-system-tweaks/00-run.sh
+++ stage-iobroker/00-system-tweaks/00-run.sh
@@ -5,16 +5,16 @@ install -m 775 files/rc.local "${ROOTFS_DIR}/etc/rc.local"
 install -m 775 files/firstboot.sh "${ROOTFS_DIR}/root/firstboot.sh"
 
 on_chroot << EOF
-	systemctl enable rc-local
+  systemctl enable rc-local
 EOF
 
 # change boot target
 on_chroot << EOF
-systemctl set-default multi-user.target
+  systemctl set-default multi-user.target
 EOF
 
 # set memory split
 on_chroot << EOF
-echo >> /boot/config.txt
-echo "gpu_mem=16" >> /boot/config.txt
+  echo >> /boot/config.txt
+  echo "gpu_mem=16" >> /boot/config.txt
 EOF
--- stage-iobroker/01-install-iobroker/00-run.sh
+++ stage-iobroker/01-install-iobroker/00-run.sh
@@ -1,21 +1,11 @@
 #!/bin/bash -e
 
-# install node
-on_chroot << EOF
-echo "Installing Node"
-curl -sLf https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
-EOF
-
 # run iobroker install script
 on_chroot << EOF
-echo "Installing ioBroker"
-curl -sLf https://iobroker.net/install.sh | sudo -E bash -
-ls -al
+  curl -sLf https://iobroker.net/install.sh | bash -
 EOF
 
 # enable iobroker autostart
 on_chroot << EOF
-echo "Enable autostart"
-update-rc.d iobroker.sh defaults
+  update-rc.d iobroker.sh defaults
 EOF
