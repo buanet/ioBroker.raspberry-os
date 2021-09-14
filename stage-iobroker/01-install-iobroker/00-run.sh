d3732f0af43d43a253f4ca4fc9bfcae4ce9a7f0c
buanet <andre@buanet.de>
2021-09-15 00:43:14 +0200

    testing

--- stage-iobroker/01-install-iobroker/00-run.sh
+++ stage-iobroker/01-install-iobroker/00-run.sh
@@ -13,12 +13,12 @@ on_chroot << EOF
 EOF
 
 # configure iobroker firstboot script
-echo "[LOG] Installing iobroker-firstboot.sh"
-install -m 775 files/iobroker-firstboot.sh "${ROOTFS_DIR}/etc/init.d/iobroker-firstboot.sh"
-on_chroot << EOF
-  update-rc.d iobroker-firstboot.sh defaults
-EOF
+# echo "[LOG] Installing iobroker-firstboot.sh"
+# install -m 775 files/iobroker-firstboot.sh "${ROOTFS_DIR}/etc/init.d/iobroker-firstboot.sh"
+# on_chroot << EOF
+#   update-rc.d iobroker-firstboot.sh defaults
+# EOF
 
 # creating file to trigger UUID reconfiguration only on first startup
-echo "[LOG] Creating checkfile /root/change_uuid"
-  touch /root/change_uuid
+# echo "[LOG] Creating checkfile /root/change_uuid"
+#  touch /root/change_uuid
