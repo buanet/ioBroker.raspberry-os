47fe3d413cd58326ba305cfede95555d198fdec9
buanet <andre@buanet.de>
2021-09-18 19:07:29 +0200

    testing

--- stage-iobroker/01-install-iobroker/00-run.sh
+++ stage-iobroker/01-install-iobroker/00-run.sh
@@ -11,3 +11,16 @@ echo "[LOG] Enabling autostart"
 on_chroot << EOF
   update-rc.d iobroker.sh defaults
 EOF
+
+# temp logging to check uuid behavior
+on_chroot << EOF
+  echo "[LOG] This is the build UUID: $(iob uuid)"
+EOF
+
+# resetting ioBroker UUID
+echo "[LOG] Resetting ioBroker UUID"
+on_chroot << EOF
+  cd /opt/iobroker
+  iob stop
+  iob unsetup -y
+EOF
