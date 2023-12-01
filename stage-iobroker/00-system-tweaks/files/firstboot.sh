57b438cd1fc93b48e4fcfe6d292cbd6f2df4343b
buanet <andre@buanet.de>
2023-12-01 13:55:37 +0100

    add hostname check

--- stage-iobroker/00-system-tweaks/files/firstboot.sh
+++ stage-iobroker/00-system-tweaks/files/firstboot.sh
@@ -5,13 +5,23 @@
 echo '>>> Starting firstboot.sh'
 
 # resize root partion to possible maximum
-echo 'Resizing root partition'
+echo -n 'Resizing root partition... '
 raspi-config nonint do_expand_rootfs
+echo 'Done.'
 
 # get current date from debian time server
-echo 'Updating date and time'
+echo -n 'Updating date and time... '
 ntpdate 0.debian.pool.ntp.org
+echo 'Done.'
+
+# check/ correct hostname in iobroker
+if [[ $(iob object get "system.adapter.admin.0" --pretty | grep -oP '(?<="host": ")[^"]*') =! $(hostname) ]]; then
+  echo -n 'Changing hostname in ioBroker... '
+  sudo -u iobroker iobroker stop
+  sudo -u iobroker iobroker host this
+  echo 'Done.'
+fi
 
 # Reboot
-echo 'Rebooting'
+echo 'Rebooting...'
 reboot
